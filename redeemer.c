6bb18c7fecd37cc89e89953a16483c881af3bd7c
Ķirzaka <102863083+kirzaka06@users.noreply.github.com>
2022-06-11 15:28:48 +0000

    oh yeah boy shake that ass whoops i mean girl girl gir girl

---
+++ redeemer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void genJak(){
+    for(int i=0;i<20;++i){
+        for(int j=0;j<40;++j){
+            printf(".");
+        }
+        printf("\n");
+    }
+}
+int main(){
+    genJak();
+}
